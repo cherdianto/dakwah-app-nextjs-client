12fb3b580d3e15299dd18e07478c735e42559d18
cherdianto <candraherdianto90@gmail.com>
2023-01-30 01:06:48 +0100

    edit materi, remove materi done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        ENV:"dev", //prod || dev
+        ENV:"prod", //prod || dev
         API_URL_PROD:"https://apimoslemguide.cherdianto.site",
         API_URL_DEV:"http://localhost:3001",
         URL_BASE_PROD:"https://moslemguide.cherdianto.site",
